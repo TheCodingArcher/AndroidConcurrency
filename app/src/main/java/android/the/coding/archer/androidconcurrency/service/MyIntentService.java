a3289728d5e705c394e784f3812aa4d48343680e
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-11-06 19:34:27 +0530

    Little cleanup

--- app/src/main/java/android/the/coding/archer/androidconcurrency/service/MyIntentService.java
+++ app/src/main/java/android/the/coding/archer/androidconcurrency/service/MyIntentService.java
@@ -4,7 +4,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
